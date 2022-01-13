21f3dcaca1b5637bd301ca14ced0a4c301fd3ac0
Murgator <38978895+Murgator@users.noreply.github.com>
2022-01-13 18:33:50 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,7 +1,7 @@
 Config = {}
 
-Config.trackyServerId = "Paste_Your_Trackyserver_ID_here"
-Config.trackyServerKey = "Paste_Your_Trackyserver_KEY_here"
+Config.trackyServerId = ""
+Config.trackyServerKey = ""
 Config.identifier = "discordid" -- discordid OR steamid
 
 --[[
@@ -16,20 +16,20 @@ Config.identifier = "discordid" -- discordid OR steamid
     the array is an array of commands to run.
 
     If the commands need them, they will be passed the following:
-        {playerid} = The server ID of the player
-        {xplayerid} = The ESX identifier of the player
-        {playername} = The name of the player
+        {playerid} = The player server ID
+        {playerlicence} = The player GTA licence
+        {playername} = The player name
 ]]
 Config.Rewards = {
     ["@"] = { -- @ = all votes
-        "t4s_addmoney {xplayerid} bank 100", -- add 100 to the player's bank account
-        "t4s_announce [VOTE] {playername} has voted and won $100 ! Number of votes: {votescount}"
+        "giveaccountmoney {playerid} bank 100", -- ESX framework command (ex_extended command)
+        "qbgivemoney {playerid} bank 100", -- QBCore framework command
+        "announce [VOTE] {playername} has voted and won $100 ! Number of votes: {votescount}. Type /vote to vote" -- (native command)
     },
     ["10"] = { -- When the player has 10 votes
-        "t4s_addmoney {xplayerid} bank 1000", -- add 1000 to the player's bank account
-        "t4s_announce [VOTE] {playername} has voted 10 times and won $1000 !"
+        "announce [VOTE] {playername} has voted 10 times !"
     },
     ["100"] = {
-        "t4s_announce [VOTE] {playername} has 100 votes !!!"
+        "announce [VOTE] {playername} has 100 votes !"
     }
 }
